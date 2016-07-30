eb827611694bd21b255d02df02782ba83261269f
Ben Demko <bendemko7@gmail.com>
2016-07-30 12:57:38 -0400

    initial commit

---
+++ Pet.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Pet
+    {
+
+        //Fields
+
+        private string name;
+        private int hunger;
+        private int thirst;
+        private int sleep;
+
+
+        //Properties
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public int Thirst
+        {
+            get { return this.thirst; }
+            set { this.thirst = value; }
+        }
+
+        public int Sleep
+        {
+            get { return this.sleep; }
+            set { this.sleep = value; }
+        }
+
+
+
+        // Constructors
+
+        public Pet()
+
+        {
+            this.Name = "Ruby";
+            this.Hunger = 50;
+            this.Thirst = 50;
+            this.Sleep = 50;
+
+        }
+
+
+        //Methods 
+
+
+        public void Feed()
+
+        {
+            Console.WriteLine("Would you like to feed {0}?", name);
+            string feedAnswer = Console.ReadLine().ToUpper();
+
+            if (feedAnswer == "YES")
+            {
+                Console.WriteLine("What would you like to feed {0} peanut butter or dog food?", name);
+                string foodChoice = Console.ReadLine();
+                Console.WriteLine("You feed {0}.", name);
+
+
+                switch (foodChoice)
+                {
+                    case "peanut butter":
+                        hunger = hunger + 10;
+                        break;
+                    case "dog food":
+                        hunger = hunger + 5;
+                        break;
+                }
+            }
+            if (feedAnswer == "NO")
+
+            {
+                hunger = hunger - 30;
+
+            }
+
+
+
+        }
+
+        //Drink Method
+
+        public void Drink()
+
+        {
+            Console.WriteLine("Would you like to give {0} water?", name);
+            string drinkAnswer = Console.ReadLine().ToUpper();
+
+            if (drinkAnswer == "YES")
+            {
+                Console.WriteLine("Woof! Woof! :)");
+                thirst = thirst + 10;
+
+            }
+            else if (drinkAnswer == "NO")
+
+            {
+                Console.WriteLine(":(");
+                thirst = thirst - 30;
+            }
+
+        }
+
+        //Sleep Method
+
+        public void Nap()
+
+        {
+            Console.WriteLine("Would you like to put {0} to bed?", name);
+            string sleepAnswer = Console.ReadLine().ToUpper();
+
+            if (sleepAnswer == "YES")
+            {
+                Console.WriteLine("Woof! Good night....Woof! :)");
+                sleep = sleep + 10;
+
+
+            }
+            else if (sleepAnswer == "NO")
+
+            {
+                Console.WriteLine(":(");
+                sleep = sleep - 30;
+            }
+
+
+        }
+
+        public void Tick()
+        {
+            hunger = hunger + 10;
+            thirst = thirst + 10;
+            sleep = sleep + 10;
+
+        }
+
+        //Runaway Method
+
+        //i.e. if hunger 
+
+
+
+
+
+        public void PrintStats()
+
+        {
+            Console.WriteLine("Hunger: " + this.hunger);
+            Console.WriteLine("Thirst: " + this.thirst);
+            Console.WriteLine("Sleep: " + this.sleep);
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to virtual pet!\nIn this game you are charged with taking care a Cairn Terrier.");
+            Pet Ben = new Pet();
+            string something;
+
+
+
+            do
+
+            {
+                Ben.PrintStats();
+                Ben.Tick();
+
+
+                Console.WriteLine("What would you like to do?  Your choices are:");
+                Console.WriteLine("1. Feed pet");
+                Console.WriteLine("2. Water pet");
+                Console.WriteLine("3. Put pet to bed");
+                Console.WriteLine("Choose a number:");
+
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    Ben.Feed();
+                }
+                else if (choice == 2)
+                {
+                    Ben.Drink();
+
+                }
+                else if (choice == 3)
+                {
+                    Ben.Nap();
+                }
+
+                else
+                {
+                    Console.WriteLine(" Choose a number between 1-3");
+                }
+
+
+                Console.WriteLine("Do you want to go again?");
+                something = Console.ReadLine();
+
+                Console.Clear();
+            }
+
+            while (something == "yes");
+
+            {
+
+                Ben.PrintStats();
+            }
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
+
